48acf7d3a3fa3054774f73b8f2e9416be695e18a
Danny <danny20030509@gmail.com>
2021-10-24 23:54:01 +0800

    Update GUI and Add schsdule window

--- Drama_Music_Controller.py
+++ Drama_Music_Controller.py
@@ -336,10 +336,10 @@ se_controls_frame.grid(row=2, column=0, pady=10)
 
 #Create music managers' frame
 bgm_managers_frame = Frame(bgm_frame)
-bgm_managers_frame.grid(row=0, column=0, sticky=E, padx=40)
+bgm_managers_frame.grid(row=0, column=0, sticky=E, padx=10)
 
 se_managers_frame = Frame(se_frame)
-se_managers_frame.grid(row=0, column=0, sticky=E, padx=40)
+se_managers_frame.grid(row=0, column=0, sticky=E, padx=10)
 
 #Create volume frame
 bgm_volume_frame = LabelFrame(bgm_frame, text="BGM Volume")
@@ -354,6 +354,7 @@ pause_image = PhotoImage(file='./img/pause.png').subsample(3)
 stop_image = PhotoImage(file='./img/stop.png').subsample(3)
 add_image = PhotoImage(file='./img/add.png').subsample(10)
 remove_image = PhotoImage(file='./img/remove.png').subsample(10)
+edit_image = PhotoImage(file='./img/edit.png').subsample(10)
 
 #Create music managers
 add_bgm_btn = Button(bgm_managers_frame, image=add_image, command=add_bgm)
@@ -427,15 +428,86 @@ se_status_bar.grid(row=4, pady=10)
 schedule_frame = LabelFrame(root, text='排程')
 schedule_frame.pack(ipadx=30, ipady=10)
 
+
+def schedule():
+    #sub-window setting
+    schedule_window = Toplevel(root)
+    schedule_window.geometry('320x320')
+    schedule_window.title('排程')
+
+    #Create title
+    title = Label(schedule_window, text='排程', font=('標楷體', 16))
+    title.pack()
+    
+    #Create controllers' frame
+    controllers_frame = Frame(schedule_window)
+    controllers_frame.pack(pady=10)
+    
+    #Create main frame
+    main_frame = Frame(schedule_window)
+    main_frame.pack(pady=10)
+    
+    #Create managers' frame
+    manager_frame = Frame(main_frame)
+    manager_frame.grid(row=0, column=1, sticky=E, padx=10)
+    
+    #Create confirm frame
+    confirm_frame = Frame(schedule_window)
+    confirm_frame.pack(pady=10)
+    
+    #Create controllers
+    minute = Entry(controllers_frame, show=None, font=('標楷體', 12), width=3)
+    minute.grid(row=0, column=0)
+    
+    colon = Label(controllers_frame, text=':', font=('標楷體', 12))
+    colon.grid(row=0, column=1)
+    
+    second = Entry(controllers_frame, show=None, font=('標楷體', 12), width=3)
+    second.grid(row=0, column=2)
+    
+    music = ttk.Combobox(controllers_frame, value=[])
+    music.grid(row=0, column=3, padx=10)
+    
+    check_btn = Button(controllers_frame, text='確定')
+    check_btn.grid(row=0, column=4)
+    
+    #Create managers
+    remove_btn = Button(manager_frame, image=remove_image)
+    remove_btn.grid(row=0, column=0)
+    
+    edit_btn = Button(manager_frame, image=edit_image)
+    edit_btn.grid(row=0, column=1)
+    
+    #Create main box
+    time_box = Listbox(main_frame, width=5)
+    time_box.grid(row=1, column=0, padx=10)
+    
+    music_box = Listbox(main_frame, width=30)
+    music_box.grid(row=1, column=1, padx=10)
+    
+    #Create confirm btns
+    confirm_btn = Button(confirm_frame, text='完成')
+    confirm_btn.grid(row=0, column=0, padx=10)
+    
+    reset_btn = Button(confirm_frame, text='重設')
+    reset_btn.grid(row=0, column=1, padx=10)
+
+
 #Create schedule controller
-schedule_btn = Button(schedule_frame, text='排程', font=('標楷體', 16))
+schedule_btn = Button(schedule_frame, text='排程', font=('標楷體', 16), command=schedule)
 schedule_btn.pack(side='left', padx=10)
 
-start_btn = Button(schedule_frame, text='開始', font=('標楷體', 16))
+pause_btn = Button(schedule_frame, image=pause_image, font=('標楷體', 16))
+pause_btn.pack(side='left', padx=10)
+
+start_btn = Button(schedule_frame, image=play_image, font=('標楷體', 16))
 start_btn.pack(side='left', padx=10)
 
+stop_btn = Button(schedule_frame, image=stop_image, font=('標楷體', 16))
+stop_btn.pack(side='left', padx=10)
+
 schedule_time_slider = ttk.Scale(schedule_frame, from_=0, to=100, orient=HORIZONTAL, value=0, length=600)
-schedule_time_slider.pack(side='left', padx=50)
+schedule_time_slider.pack(side='left', padx=30)
 
 schedule_status_bar = Label(schedule_frame, text='', bd=1, relief=GROOVE, anchor=E)
 schedule_status_bar.pack(side='left', padx=10)
